d3f6e84c2a06d6ae21cd5bb55fd26594bfed9e19
Cindy <cspanameno2013@gmail.com>
2017-01-31 10:36:14 -0800

    addded dynamic route

--- app/router.js
+++ app/router.js
@@ -2,11 +2,22 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+  location: config.locationType
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('bookmarks', function() {
+    this.route('new');
+
+    this.route('edit', {
+      path: '/edit/:bookmark_id'
+    });
+  });
+
+  this.route('page-not-found', {
+    path: '/*wildcard'
+  });
 });
 
 export default Router;
